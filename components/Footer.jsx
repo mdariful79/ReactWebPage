435ebba45fe592c4eb80f291d6b481e9242d69e7
mdariful79 <mdariful057@gmail.com>
2024-12-01 15:26:36 +0600

    completed the web page

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ariful development. All rights reserved.</small>
+        </footer>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="src/assets/react.svg" className="nav-logo" alt="React logo" />
+                <span>Reactfacts</span>
+            </nav>
+        </header>
+    )
+}
